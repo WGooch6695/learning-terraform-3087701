69f29182f46a2b8537daee7379f7affdcfcfb55d
WGooch6695 <WGooch6695@gmail.com>
2023-01-22 18:54:21 -0500

    updated dev vpc code

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "app_ami" {
 }
 
 # Create public vpc using the vpc module
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -50,7 +50,7 @@ module "blog_sg" {
   version = "4.17.1"
   name    = "blog"
 
-  vpc_id  = module.vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
